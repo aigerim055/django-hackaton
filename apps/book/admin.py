c3f7ff78527d9fd1d729d0a49e44e6d1bf28e0f1
adinaim <adinaitm@gmail.com>
2022-11-16 20:49:00 +0600

    admin error

--- apps/book/admin.py
+++ apps/book/admin.py
@@ -9,7 +9,7 @@ from django.apps import apps
 from django.contrib import admin
 from django.contrib.admin.sites import AlreadyRegistered
 
-app_models = apps.get_app_config('apps.book').get_models()
+app_models = apps.get_app_config('book').get_models()
 for model in app_models:
     try:
         admin.site.register(model)
