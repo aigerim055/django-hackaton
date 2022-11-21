e625ee9a15eef584b42fa3a01698818cef8fea96
suyumbaevaaigerim <suyumbaevaaigerim@gmail.com>
2022-11-21 20:04:55 +0600

    password

--- apps/account/urls.py
+++ apps/account/urls.py
@@ -6,7 +6,10 @@ from rest_framework_simplejwt.views import (
 
 from .views import (
     RegistrationView,
-    ActivationView
+    ActivationView,
+    ChangePasswordView,
+    SetRestoredPasswordView,
+    RestorePasswordView
 )
 
 
@@ -15,4 +18,7 @@ urlpatterns = [
     path('activate/', ActivationView.as_view(), name='activate'),
     path('login/', TokenObtainPairView.as_view(), name='login'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),   
+    path('change-password/', ChangePasswordView.as_view(), name='change-password'),
+    path('restore-password/', RestorePasswordView.as_view(), name='restore_pasword'),
+    path('set-restored-password/', SetRestoredPasswordView.as_view(), name='set_restored_password'),
 ]
--- apps/account/views.py
+++ apps/account/views.py
@@ -2,10 +2,14 @@ from rest_framework.views import APIView
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 
 from .serializers import (
     RegistrationSerializer,
     ActivationSerializer,
+    ChangePasswordSerializer,
+     RestorePasswordSerializer,
+     SetRestoredPasswordSerializer,
     )
 
 
@@ -28,5 +32,39 @@ class ActivationView(APIView):
             serilizer.activate_account()
             return Response(
                 'Аккаунт активирован!',
+                status=status.HTTP_200_OK
+            )
+
+
+class ChangePasswordView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request: Request):
+        serializer = ChangePasswordSerializer(data=request.data, context={'request': request})
+        if serializer.is_valid(raise_exception=True):
+            serializer.set_new_password()
+            return Response(
+                'password changed seccesfully!',
+                status=status.HTTP_200_OK
+            )
+
+
+class RestorePasswordView(APIView):
+    def post(self, request):
+        serializer = RestorePasswordSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.send_code()
+            return Response(
+                'code was sent to your phone',
+                status=status.HTTP_200_OK
+            )
+
+class SetRestoredPasswordView(APIView):
+    def post(self, request: Request):
+        serializer = SetRestoredPasswordSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.set_new_password()
+            return Response(
+                'password restored successfuly',
                 status=status.HTTP_200_OK
             )
